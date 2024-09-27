7b6365f263004a426da0318561736eeca11c07ff
Brian Lau <130273874+seldomearly69@users.noreply.github.com>
2024-09-27 14:14:49 +0800

    included managers' positions in the resolve manager list by director endpoint

--- backend/requests/app.py
+++ backend/requests/app.py
@@ -119,9 +119,14 @@ class DepartmentSchedule(graphene.ObjectType):
     department_name = graphene.String()
     dept_schedule = graphene.List(TeamSchedule)
 
+
+class Manager(graphene.ObjectType):
+    staff_id = graphene.Int()
+    position = graphene.String()
+
 class ManagerList(graphene.ObjectType):
     director_name = graphene.String()
-    manager_list = graphene.List(graphene.Int)
+    manager_list = graphene.List(Manager)
 
 class Query(graphene.ObjectType):
 
@@ -178,8 +183,8 @@ def resolve_manager_list(director_id):
         raise Exception("User is not a director. This endpoint is for directors only")
     m_list = User.query.filter(User.reporting_manager == director_id).all()
     return {
-        "director": user.staff_fname + " " + user.staff_lname,
-        "manager_list": [m.staff_id for m in m_list]
+        "director_name": user.staff_fname + " " + user.staff_lname,
+        "manager_list": [{"staff_id":m.staff_id, "position": m.position} for m in m_list]
     }
 
 def resolve_own_schedule(month, year, staff_id):
