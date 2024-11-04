731256b4f8ad4fcb813ae5dfbfb1e3989d54e674
sakthivelg2022 <149075734+sakthivelg2022@users.noreply.github.com>
2024-11-04 09:32:48 +0800

    CI fix

--- backend/requests/unit_tests.py
+++ backend/requests/unit_tests.py
@@ -65,6 +65,7 @@ class BaseTestCase(unittest.TestCase):
         }
         app.config['TESTING'] = True
         cls.client = app.test_client()
+        cls.client.environ_base['HTTP_USER_AGENT'] = 'custom-agent/1.0'
 
         # Patch external services
         cls.amqp_patcher = patch('amqp_connection.create_connection', return_value=mock_connection)
